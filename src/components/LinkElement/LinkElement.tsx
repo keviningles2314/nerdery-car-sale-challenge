236434e2a7013789a112394e8b056d60d72ed24c
keviningles2314 <kevin.ingles2314@gmail.com>
2022-09-01 16:45:43 -0600

    feat: add LinkElement component

---
+++ src/components/LinkElement/LinkElement.tsx
@@ -0,0 +1,15 @@
+import RegularText from '../Text/RegularText/RegularText';
+import { LinkElementStyled } from './LinkElementStyled';
+interface LinkElementProps {
+  path: string;
+  text: string;
+}
+const LinkElement = ({ path, text }: LinkElementProps) => {
+  return (
+    <LinkElementStyled to={path}>
+      <RegularText text={text} isBaseColor={false} />
+    </LinkElementStyled>
+  );
+};
+
+export default LinkElement;
