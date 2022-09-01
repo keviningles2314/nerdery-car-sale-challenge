6812abe9835e0267668e5772e14f2b96ee71d3e6
keviningles2314 <kevin.ingles2314@gmail.com>
2022-09-01 16:07:45 -0600

    style: add global styles

---
+++ src/style/theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+  white: '#ffffff',
+  black: '#202020',
+  blue: '#2c3e50',
+  yellow: '#f39c12',
+  big: '3vh',
+  regular: '2vh',
+  small: '1vh',
+  rounded: '4px',
+  gray: '#7f8c8d',
+};
