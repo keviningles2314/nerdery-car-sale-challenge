eb62071d2c38a383469bc29f09044ede8b99b38e
keviningles2314 <63429867+keviningles2314@users.noreply.github.com>
2022-10-14 17:55:28 -0600

    Delete src/pages/Home directory

--- src/pages/Home/Home.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react"
-import Home from "./home"
-
-describe("Test Home Page", () => {
-  it("should render home page", () => {
-    render(<Home />)
-    expect(screen.getByText(/welcome/i)).toBeInTheDocument()
-  })
-})
